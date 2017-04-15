2e060a4b3230a88bb1f65d5ab157d3a6ffe97202
Satya Jayanti <sjayanti@redhat.com>
2017-04-15 21:36:15 +0530

    camel-webservice Fuse 6.2.1 project
    This is the SOAP web service project build on Fuse 6.2.1 that needs to
    be migrated to FIS 2.0 in Module 3 lab.

---
+++ camel-webservice/target/generated/src/main/java/com/redhat/gpe/training/SaveCustomer.java
@@ -0,0 +1,62 @@
+
+package com.redhat.gpe.training;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for saveCustomer complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="saveCustomer"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="customer" type="{http://training.gpe.redhat.com/}customer"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "saveCustomer", propOrder = {
+    "customer"
+})
+public class SaveCustomer {
+
+    @XmlElement(required = true)
+    protected Customer customer;
+
+    /**
+     * Gets the value of the customer property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Customer }
+     *     
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * Sets the value of the customer property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Customer }
+     *     
+     */
+    public void setCustomer(Customer value) {
+        this.customer = value;
+    }
+
+}
